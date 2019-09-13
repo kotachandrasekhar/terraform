6f7bd751129d218d92baa8caffb084841dc60bc1
KOTA CHANDRA SEKHARA  MANIKANTA <22449602+kotachandrasekhar@users.noreply.github.com>
2019-09-13 09:54:19 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,4 +2,6 @@ sudo apt-get install openjdk-8-jdk -y
 sudo wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 sudo apt-get install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
-sudo apt-get update && sudo apt-get install elasticsearch
+sudo apt-get update && sudo apt-get install elasticsearch 
+sudo apt-get update && sudo apt-get install logstash
+sudo apt-get update && sudo apt-get install kibana
