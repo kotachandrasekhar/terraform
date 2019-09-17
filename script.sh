c994224186159fa59eac382eaa276b59fecb883c
KOTA CHANDRA SEKHARA  MANIKANTA <22449602+kotachandrasekhar@users.noreply.github.com>
2019-09-17 06:11:23 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,12 +2,12 @@ sudo su
 sudo apt-get install openjdk-8-jdk -y
 sudo wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 sudo apt-get install apt-transport-https
-echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
+sudo echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
 sudo apt-get update && sudo apt-get install elasticsearch 
 sudo apt-get update && sudo apt-get install logstash
 sudo apt-get update && sudo apt-get install kibana
-echo 'export ip='$(hostname -i)'' >> ~/.profile
-source ~/.profile
+sudo echo 'export ip='$(hostname -i)'' >> ~/.profile
+sudo source ~/.profile
 sed -i '/#cluster.name: my-application/c\cluster.name: my-cluster' /etc/elasticsearch/elasticsearch.yml
 sed -i '/#node.name: node-1/c\node.name: test' /etc/elasticsearch/elasticsearch.yml
 sed -i 's/#network.host: 192.168.0.1/network.host: '$ip'/' /etc/elasticsearch/elasticsearch.yml
