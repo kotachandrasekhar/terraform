53bc9d2a71ee49d74b03b5ec26050d5b643558f9
KOTA CHANDRA SEKHARA  MANIKANTA <22449602+kotachandrasekhar@users.noreply.github.com>
2019-09-17 08:58:28 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,6 +8,8 @@ sudo apt-get update && sudo apt-get install logstash
 sudo apt-get update && sudo apt-get install kibana
 echo 'export ip='$(hostname -i)'' >> ~/.profile
 source ~/.profile
+echo 'export ip='$(hostname -i)'' >> ~/.bashrc
+source ~/.bashrc
 sed -i '/#cluster.name: my-application/c\cluster.name: my-cluster' /etc/elasticsearch/elasticsearch.yml
 sed -i '/#node.name: node-1/c\node.name: test' /etc/elasticsearch/elasticsearch.yml
 sed -i 's/#network.host: 192.168.0.1/network.host: '$ip'/' /etc/elasticsearch/elasticsearch.yml
